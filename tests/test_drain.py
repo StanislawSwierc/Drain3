5577e62cc33a9118a8972882f56cca595e14320a
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2021-01-12 16:16:05 -0800

    Break more tests

--- tests/test_drain.py
+++ tests/test_drain.py
@@ -59,7 +59,7 @@ class DrainTest(unittest.TestCase):
         expected = str.splitlines(
             """
             broken
-            Dec 10 07:08:28 LabSZ sshd[24208]: input_userauth_request: invalid user webmaster [preauth]
+            broken
             Dec 10 09:12:32 LabSZ sshd[24490]: Failed password for invalid user ftpuser from 0.0.0.0 port 62891 ssh2
             Dec 10 <*> LabSZ <*> Failed password for invalid user <*> from 0.0.0.0 port <*> ssh2
             Dec 10 <*> LabSZ <*> Failed password for invalid user <*> from 0.0.0.0 port <*> ssh2
